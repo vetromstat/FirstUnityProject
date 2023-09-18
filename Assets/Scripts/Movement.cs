e067466a8023e629f35f4c20f03b7a69467852a8
vetromstat <kravtsov333@gmail.com>
2023-09-18 18:20:26 +0300

    Turning with the mouse
    Added turning with the mouse

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,25 +6,35 @@ using UnityEngine;
 public class TopDownCharacterMover : MonoBehaviour
 {
 
-
     [SerializeField]
     private float movementSpeed;
+    [SerializeField]
 
 
     // Update is called once per frame
     void Update()
     {
         HandleMovementInput();
-
+        HandleRotationInput();
     }
     void HandleMovementInput()
     {
 
-    float horizontal = Input.GetAxis("Horizontal");
-    float vertical = Input.GetAxis("Vertical");
-    Vector3 movement = new Vector3(horizontal, 0, vertical);
-    transform.Translate(movement* movementSpeed* Time.deltaTime, Space.World);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        transform.Translate(movement* movementSpeed* Time.deltaTime, Space.World);
 
+
+    }
+    void HandleRotationInput()
+    {
+        RaycastHit _hit;
+        Ray _ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(_ray,out _hit))
+        {
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+        }
     }
 
 }
